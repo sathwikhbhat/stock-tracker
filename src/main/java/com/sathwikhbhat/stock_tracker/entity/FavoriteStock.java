720105fd0ed1357169c275d1ba2ba98ab35f6ba2
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-09-27 18:02:15 +0530

    fix: correct casing of 'Id' to 'id' in FavoriteStock entity

--- src/main/java/com/sathwikhbhat/stock_tracker/entity/FavoriteStock.java
+++ src/main/java/com/sathwikhbhat/stock_tracker/entity/FavoriteStock.java
@@ -21,7 +21,7 @@ public class FavoriteStock {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
 
     @Column(nullable = false, unique = true)
     private String symbol;
